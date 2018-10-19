9caf1120a0d6461c7fce571ed5070cf78a1cde2f
charkevi391 <charkevi391@ed.amdsb.ca>
2018-10-19 15:04:18 -0400

    3R

--- Pizza Parlor n/Form1.cs
+++ Pizza Parlor n/Form1.cs
@@ -7,6 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+/// Created by: Charlie Kevill
+/// Completed: 10/15/2018
+/// Description: A basic cashregister for A burger resturant.
 
 namespace Pizza_Parlor_n
 {
@@ -25,6 +31,7 @@ namespace Pizza_Parlor_n
         double subTotal;
         double ammountTendered;
         double changeGiven;
+
 
         public BigBurgerBar()
         {
@@ -69,7 +76,7 @@ namespace Pizza_Parlor_n
                 totalCost = subTotal * HST_TAX + subTotal;
                 ammountTendered = Convert.ToDouble(tenderedBox.Text);
                 changeGiven = ammountTendered - totalCost;
-                label14.Text = "$" + changeGiven;
+                label14.Text = changeGiven.ToString("C");
             }
 
             catch
@@ -85,9 +92,10 @@ namespace Pizza_Parlor_n
             SolidBrush whiteBrush = new SolidBrush(Color.White);
             SolidBrush blackBrush = new SolidBrush(Color.Black);
             Font titleFont = new Font("Times New Roman", 25, FontStyle.Bold);
-            Font boldFont = new Font("Times New Roman", 15, FontStyle.Bold);
-            Font normalFont = new Font("Times New Roman", 15);
-            Font bigFont = new Font("Times New Roman", 30);
+            Font boldFont = new Font("Consolas", 15, FontStyle.Bold);
+            Font normalFont = new Font("Consolas", 15);
+            Font bigFont = new Font("Consolas", 30);
+            SoundPlayer player = new SoundPlayer(Properties.Resources.Test);
 
             burgersPurchased = Convert.ToDouble(burgerBox.Text);
             friesPurchased = Convert.ToDouble(friesBox.Text);
@@ -101,18 +109,43 @@ namespace Pizza_Parlor_n
             g.DrawRectangle(blackPen, 600, 20, 400, 500);
 
             g.DrawString("Big Burger Bar", normalFont, blackBrush, 620, 40);
+            player.Play();
+            Thread.Sleep(1000);
             g.DrawString("Oct, 12th, 2018", normalFont, blackBrush, 620, 60);
+            player.Play();
+            Thread.Sleep(1000);
             g.DrawString("Order #1017", normalFont, blackBrush, 620, 80);
+            player.Play();
+            Thread.Sleep(1000);
             g.DrawString("Big Burger Bar", normalFont, blackBrush, 620, 100);
-            g.DrawString("Hamburgers  x" + burgerBox.Text + " @ 2.49" , normalFont, blackBrush, 620, 120);
-            g.DrawString("Fries x" + friesBox.Text + " @ 1.89", normalFont, blackBrush, 620, 140);
-            g.DrawString("Drinks  x" + drinkBox.Text + " @ 0.99", normalFont, blackBrush, 620, 160);
-            g.DrawString("SubTotal  $" + subTotal, normalFont, blackBrush, 620, 180);
-            g.DrawString("Tax  $" + taxTotal, normalFont, blackBrush, 620, 200);
-            g.DrawString("Total  $" + totalCost, normalFont, blackBrush, 620, 220);
-            g.DrawString("Tendered  $" + ammountTendered, normalFont, blackBrush, 620, 240);
-            g.DrawString("Change  $" + changeGiven, normalFont, blackBrush, 620, 260);
-            g.DrawString("Thanks for choosing Big Burger Bar! :) ", normalFont, blackBrush, 620, 280);
+            player.Play();
+            Thread.Sleep(1000);
+            g.DrawString("Hamburgers        x" + burgerBox.Text + " @ 2.49" , normalFont, blackBrush, 620, 120);
+            player.Play();
+            Thread.Sleep(1000);
+            g.DrawString("Fries             x" + friesBox.Text + " @ 1.89", normalFont, blackBrush, 620, 140);
+            player.Play();
+            Thread.Sleep(1000);
+            g.DrawString("Drinks            x" + drinkBox.Text + " @ 0.99", normalFont, blackBrush, 620, 160);
+            player.Play();
+            Thread.Sleep(1000);
+            g.DrawString("SubTotal          $" + subTotal, normalFont, blackBrush, 620, 180);
+            player.Play();
+            Thread.Sleep(1000);
+            g.DrawString("Tax               $" + taxTotal, normalFont, blackBrush, 620, 200);
+            player.Play();
+            Thread.Sleep(1000);
+            g.DrawString("Total             $" + totalCost, normalFont, blackBrush, 620, 220);
+            player.Play();
+            Thread.Sleep(1000);
+            g.DrawString("Tendered          $" + ammountTendered, normalFont, blackBrush, 620, 240);
+            player.Play();
+            Thread.Sleep(1000);
+            g.DrawString("Change            $" + changeGiven, normalFont, blackBrush, 620, 260);
+            player.Play();
+            Thread.Sleep(1000);
+            g.DrawString("Thanks for eating here! :) ", normalFont, blackBrush, 620, 280);
+            player.Play();
 
         }
     }
